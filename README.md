866fcad67c2479adbb81d2d707d4ab66856bbad7
joe <youssef21ab@outlook.com>
2024-07-25 01:19:47 +0300

    V1 Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project-1-Landing-Page
-My own landing page. This is also my first task @ headstarter ai fellowship
---
+++ js/custom.js
@@ -0,0 +1,94 @@
+$(function () {
+        "use strict";
+
+        // MENU
+        $(".navbar .nav-link").on("click", function () {
+            $(".navbar-collapse").collapse("hide");
+        });
+
+        $(window).on("scroll", function () {
+            /*----------------------------------------------------*/
+            /*  Navigtion Menu Scroll
+                /*----------------------------------------------------*/
+
+            var b = $(window).scrollTop();
+
+            if (b > 72) {
+                $(".navbar").addClass("scroll");
+            } else {
+                $(".navbar").removeClass("scroll");
+            }
+        });
+
+        // SMOOTHSCROLL
+        $(function () {
+            $(" .navbar .nav-link").on("click", function (event) {
+                const $anchor = $(this);
+                $("html, body")
+                    .stop()
+                    .animate({
+                        scrollTop: $($anchor.attr("href")).offset().top - 49,
+                    }, 1000);
+                event.preventDefault();
+            });
+        });
+
+        const txt = "👋 I'm Youssef"; /* The initial text */
+        const speed = 75; /* The speed/duration of the effect in milliseconds */
+        let i = 0;
+
+        $(function typeWriter() {
+            if (document.getElementById("TypeWriter").innerHTML.length < txt.length) {
+                document.getElementById("TypeWriter").innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            } else {
+                typeWriter2(0, 0); // Start the next phase
+            }
+        })
+
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        async function clearText() {
+            let length = document.getElementById("TypeWriter").innerHTML.length;
+            console.log(`clearText() called, Length: ${length}`);
+            if (length > txt.length) {
+                document.getElementById("TypeWriter").innerHTML = document
+                    .getElementById("TypeWriter")
+                    .innerHTML.slice(0, length - 1);
+                await sleep(speed);
+                await clearText();
+            }
+        }
+
+        async function typeWriter2(idx, i) {
+            const messages = [", A Fullstack Dev", ", An Aspiring Computer Engineer", ", A Security Researcher", ", A Tech Enthusiast", ", A ML Enthusiast", ", A Cybersecurity Enthusiast",];
+            let message = messages.at(idx);
+
+            // console.log(`message: ${message}, idx:${idx}, i:${i}`);
+
+            if (idx < messages.length && document.getElementById("TypeWriter").innerHTML.length < txt.length + message.length) {
+                document.getElementById("TypeWriter").innerHTML += message.at(i);
+                i++;
+            } else if (idx < messages.length - 1) {
+                await sleep(300); // Wait for 1000ms before clearing
+                await clearText(); // Clear the text
+                i = 0;
+                idx++;
+            } else
+                return;
+
+
+            await sleep(speed); // Wait for the typing speed
+            await typeWriter2(idx, i); // Recurse
+        }
+
+    }
+)
+
+//Dark Mode
+function enableDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
